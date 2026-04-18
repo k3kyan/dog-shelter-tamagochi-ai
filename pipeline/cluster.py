04a18c3f4f2e71d260a2240a1eafa4b2416924bd
k3kyan <90981513+k3kyan@users.noreply.github.com>
2026-04-18 00:19:34 -0500

    setup venv + forgot unsaved comments in cluster.py

--- pipeline/cluster.py
+++ pipeline/cluster.py
@@ -1,3 +1,7 @@
 # KMeans clustering
 # actually im gonna do this later bc its kinda hard, need to research more, maybe implement later
 # for data engineering and ml engineering practice, good, but hard
+# math is hard...
+
+# to research
+# elbow method, silhouette scoring, standardscalar, feature selection decisions, inertia??? 
