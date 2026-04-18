e9733f7b4ba6a55bd9bf986f7e574963cde9132c
k3kyan <90981513+k3kyan@users.noreply.github.com>
2026-04-17 22:34:44 -0500

    create pipeline orchestrator

---
+++ pipeline/run_pipeline.py
@@ -0,0 +1,13 @@
+import subprocess, logging #subprocess module lets python run external commands, like running commands in a terminal or other scripts
+logging.basicConfig(level=logging.INFO)
+
+subprocess.run(['python', 'clean.py'], check=True) # run 'python clean.py' command in terminal
+# subprocess.run(['python', 'cluster.py'], check=True) # if i add kmeans clustering later
+
+
+# this file orchestrates the pipeline's workflow
+
+# How to run full pipeline:
+# cd pipeline
+# source pipelinevenv/bin/activate
+# python run_pipeline.py
