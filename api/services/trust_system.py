ccb9810b58091d41de9d6220ec41520dad2d185e
k3kyan <90981513+k3kyan@users.noreply.github.com>
2026-04-19 15:31:07 -0500

    add actions to trust system

--- api/services/trust_system.py
+++ api/services/trust_system.py
@@ -8,7 +8,8 @@ TRUST_GAINS = {
     'feed': 7, 
     'groom': 3, 
     'talk': 5,
-    'ignore': -1
+    'ignore': -1,
+    'rest': 0
 }
 
 # TODO-later: could probably put these stage names as enums
