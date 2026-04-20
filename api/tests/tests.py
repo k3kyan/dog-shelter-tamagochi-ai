8b4b5d2955bbff69fb61a8a5acf0f020c233695e
k3kyan <90981513+k3kyan@users.noreply.github.com>
2026-04-20 02:52:37 -0500

    tests on hold

--- api/tests/tests.py
+++ api/tests/tests.py
@@ -3,3 +3,4 @@ from fastapi.testclient import TestClient
 
 # test by running this in terminal:
 # pytest tests/ -v
+# TODO: actually i will do tests later bc i already tested with swaggerUI and docker dynamodblocal and everything worked
