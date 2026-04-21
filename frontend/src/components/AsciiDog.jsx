d77c91e88140929c31318d5cddd49bb7754bbb1e
k3kyan <90981513+k3kyan@users.noreply.github.com>
2026-04-20 20:18:14 -0500

    replaced ascii w dog images

--- frontend/src/components/AsciiDog.jsx
+++ frontend/src/components/AsciiDog.jsx
@@ -1,22 +1,40 @@
-const ASCII = {
-  scared: "            __\n      (___()';;\n      /,    /'\n      \\\\--\\\\",
-  cautious: "            __\n      (___()'`;\n      /,    /'\n      \\\\--\\\\",
-  curious: "            __\n      (___()'?\n      /,    /'\n      \\\\--\\\\",
-  happy: "            __\n      (___()'`;\n      /,  ^  /'\n      \\\\--\\\\",
-  thriving: "            __\n      (___()'`;\n      /,  ^  /'\n      \\\\~~\\\\",
-  sad: "            __\n      (___()'.;\n      /,    /'\n      \\\\--\\\\",
-  sick: "            __\n      (___()'x;\n      /,    /'\n      \\\\--\\\\",
-};
+import scaredImg   from '../assets/scared.png'
+import cautiousImg from '../assets/cautious.png'
+import curiousImg  from '../assets/curious.png'
+import happyImg    from '../assets/happy.png'
+import thrivingImg from '../assets/thriving.png'
+import sadImg      from '../assets/sad.png'
+import sickImg     from '../assets/sick.png'
+
+const IMAGES = {
+  scared:   scaredImg,
+  cautious: cautiousImg,
+  curious:  curiousImg,
+  happy:    happyImg,
+  thriving: thrivingImg,
+  sad:      sadImg,
+  sick:     sickImg,
+}
+
+// const ASCII = {
+//   scared: "            __\n      (___()';;\n      /,    /'\n      \\\\--\\\\",
+//   cautious: "            __\n      (___()'`;\n      /,    /'\n      \\\\--\\\\",
+//   curious: "            __\n      (___()'?\n      /,    /'\n      \\\\--\\\\",
+//   happy: "            __\n      (___()'`;\n      /,  ^  /'\n      \\\\--\\\\",
+//   thriving: "            __\n      (___()'`;\n      /,  ^  /'\n      \\\\~~\\\\",
+//   sad: "            __\n      (___()'.;\n      /,    /'\n      \\\\--\\\\",
+//   sick: "            __\n      (___()'x;\n      /,    /'\n      \\\\--\\\\",
+// };
 
 export default function AsciiDog({ mood = 'scared', chatBubble = null }) {
-  const art = ASCII[mood] || ASCII.scared
+  const src = IMAGES[mood] || IMAGES.scared
 
   return (
     <div className="ascii-dog-container">
       {chatBubble && (
         <div className="chat-bubble">{chatBubble}</div>
       )}
-      <pre className="ascii-dog">{art}</pre>
+      <img src={src} alt={mood} className="dog-img" />
     </div>
   )
 }
